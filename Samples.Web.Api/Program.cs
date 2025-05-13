245df43bc5d49fd74ed00685d7aa91d28350abab
cr1pto <20729543+cr1pto@users.noreply.github.com>
2025-05-12 22:11:30 -0700

    adjust Authority url

--- Samples.Web.Api/Program.cs
+++ Samples.Web.Api/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication()
     .AddJwtBearer(options =>
     {
-        options.Authority = "https://localhost:5001";
+        options.Authority = "https://localhost:44385";
         options.TokenValidationParameters.ValidateAudience = false;
     });
 builder.Services.AddAuthorization(options =>
